da0e884389ec737bbb6ada1f46ddc52ec5f6adb4
Feyisayo Victor <73366979+FeyiAdeaga@users.noreply.github.com>
2021-11-03 16:32:31 +0000

    Whole thing

---
+++ Minesweeper/minesweeper.py
@@ -0,0 +1,4 @@
+from minesweeper_functions import play
+
+if __name__ == "__main__":
+    play()
--- Tic Tac Toe/tic_tac_toe.py
+++
@@ -1,5 +0,0 @@
-from tic_tac_toe_functions import draw_board, print_board
-
-
-board = draw_board(3,3)
-print_board(board)
